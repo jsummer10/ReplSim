b31180713b09ec99c3b8777e9f0b10db7f7396af
martinlopez <martin.m.lopez@azcarnivores.com>
2021-04-21 23:54:44 -0700

    ann now completes training. issues with testing. working on transform and scaling test set.

--- run.py
+++ run.py
@@ -8,29 +8,30 @@ Description : The file contains the main functions to run
               the simulations 
 '''
 
-__softwarename__    = 'Replacement Policy Simulation'
-__author__          = 'Jake Summerville, Henry Lee, Martin Lopez, Fausto Sanchez'
-__copyright__       = 'Copyright (C) 2021 Jake Summerville, Henry Lee, Martin Lopez, Fausto Sanchez'
-__credits__         = ['Jake Summerville, Henry Lee, Martin Lopez, Fausto Sanchez']
-__version__         = '1.0.0'
-__status__          = 'Development'
-__doc__             = 'The Replacement Policy Simulator helps simulate a fully associative or set \
+__softwarename__ = 'Replacement Policy Simulation'
+__author__ = 'Jake Summerville, Henry Lee, Martin Lopez, Fausto Sanchez'
+__copyright__ = 'Copyright (C) 2021 Jake Summerville, Henry Lee, Martin Lopez, Fausto Sanchez'
+__credits__ = ['Jake Summerville, Henry Lee, Martin Lopez, Fausto Sanchez']
+__version__ = '1.0.0'
+__status__ = 'Development'
+__doc__ = 'The Replacement Policy Simulator helps simulate a fully associative or set \
                        associative cache to estimate the hit rate for various replacement policies'
 
-import os,sys, time
+import os, sys, time
+
 sys.path.insert(0, os.getcwd() + os.path.sep + 'src')
 
 from src.simulation import Simulation
-from src.logger     import InitializeLogger
-from src.arguments  import ReadArguments
+from src.logger import InitializeLogger
+from src.arguments import ReadArguments
+
 
 def main():
-
-    # Save initial start time 
+    # Save initial start time
     start_time = time.time()
 
     InitializeLogger()
-  
+
     ReadArguments()
 
     Simulation()
@@ -38,6 +39,7 @@ def main():
     time_dif = (time.time() - start_time) / 60
 
     print('\nTesting Complete ({:.2f} minutes)\n'.format(time_dif))
+
 
 if __name__ == '__main__':
     main()
--- src/ann.py
+++ src/ann.py
@@ -13,111 +13,103 @@ import pandas as pd
 import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
-from pandas import DataFrame
-from pandas import concat
-from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout, GRU, Bidirectional
 from sklearn.preprocessing import MinMaxScaler
-from keras.optimizers import SGD
 import math
 from sklearn.metrics import mean_squared_error
 
-os.chdir("../output/")
-
-
-# ----------------------------------------------------------------------
-#     import the cache data replacement information
-#     convert hexadecimal into int for LSTM friendly processing
-# ----------------------------------------------------------------------
-
-
-def ConvertData():
-    # -----------------------------------------------------------------
-    #     converts replacement policy results into int for processing in
-    #     neural net
-    # ------------------------------------------------------------------
-
-    df = pd.read_excel('replacement_policy_data.xlsx', index_col=None, engine='openpyxl', header=0)
-    df['cache_addr'] = df['cache_addr'].apply(lambda x: int(x, 16))
-    df.to_csv('data_int_replacement.csv', index=False)
-
-    # -----------------------------------------------------------------
-    # plot to help us visualize the data
-    # this should plot our predicted replacement policy over time
-    # -----------------------------------------------------------------
+os.chdir("../mem/")
 
 
 def Plot_Predictions(test, predicted):
     plt.plot(test, color='red', label='Real Policy Replacement')
     plt.plot(predicted, color='blue', label='Predicted Replacement Policy')
     plt.title('Replacement Policy Prediction')
-    plt.xlabel('cache_addr') # want to predict patterns of memory hits
+    plt.xlabel('cache_addr')  # want to predict patterns of memory hits
     plt.ylabel('Replacement Policy Prediction')
     plt.legend()
     plt.show()
 
-    # -----------------------------------------------------------------
-    #     Calculate the Root Mean Square Error
-    # -----------------------------------------------------------------
 
-
+# -----------------------------------------------------------------
+# Calculate the Root Mean Square Error
+# -----------------------------------------------------------------
 def Return_RMSE(test, predicted):
     rmse = math.sqrt(mean_squared_error(test, predicted))
     print("The root mean squared error is {}.".format(rmse))
 
 
 def Train_Replacement_Policy():
-    dataset = pd.read_csv("data_int_replacement.csv", index_col='policy')
-    dataset.head()
+    # data set column [0] = address
+    # column[1] = index + tag
+    dataset = pd.read_csv("gen_mem.csv", header=None)
+    df = pd.DataFrame(dataset)
+    # print("data set:")
+    # print(df.head())
     # -------------------------------------------------------------------
-    #      Checking for recurring cache addresses to train on to enable replacement policy
-    #      the training set and the test set. We are looking at testing and training
-    #      what we can predict the cache addresses and forecast to optimize our cache
-    # -------------------------------------------------------------------
-
-    training_set = dataset[:'cache_addr'].iloc[:1].values
-    print(training_set)
-    test_set = dataset['cache_addr':].iloc[:1].values
-    print(test_set)
-
-    # -------------------------------------------------------------------
-    # come up with math here
+    # Checking for recurring cache addresses to train on to enable replacement policy
+    # the training set and the test set. We are looking at testing and training
+    # what we can predict the cache addresses and forecast to optimize our cache
     # -------------------------------------------------------------------
 
-    dataset["class"].plot(figsize=(16, 4), legend=True)
-    plt.legend(['Training set Hits', 'Test set Misses']) # update to cache addr
+    # getting the index + tag
+    training_set = df.values
+    # print("Training Set:")
+    # print(training_set)
+    test_set = df.values
+    # print("Test Set:)")
+    # print(test_set)
+
+    # -------------------------------------------------------------------
+    # plot sequence for prediction
+    # plot demonstrates predictability and sequence in index + tag
+    # -------------------------------------------------------------------
+    df[1][:300].plot(figsize=(16, 4), legend=True)
+    plt.legend(['Test set Index + Tag'])
     plt.title('L1 Smart Cache')
-    plt.show()
-    # --------------------------------------------------------------------
-    #      scale our training set
-    # --------------------------------------------------------------------
+    # plt.show()
+
+    # # --------------------------------------------------------------------
+    # scale our training set from zero to 1 for training set
+    # scaled training set will be used to train
+    # # --------------------------------------------------------------------
 
     sc = MinMaxScaler(feature_range=(0, 1))
     training_set_scaled = sc.fit_transform(training_set)
+    # print(training_set_scaled)
 
     # --------------------------------------------------------------------
-    #     Long Short-Term Memory stores long term memory state
-    #
+    # Long Short-Term Memory stores long term memory state
+    # Sequence
     # --------------------------------------------------------------------
-
     X_train = []
     y_train = []
-    for i in range(30, 500):
-        X_train.append(training_set_scaled[i - 500:i, 0])
-        y_train.append(training_set_scaled[i, 0])
-        X_train, y_train = np.array(X_train), np.array(y_train)
 
     # --------------------------------------------------------------------
-    #     Long Short-Term Memory (LSTM) architecture
-    #     we have four LSTM layers
-    #     we have one output layer
+    # ranging from 28 - length of training set
+    # chosen 28 as 28 bits for 2-way associative cache as baseline
+    # can choose different number
     # --------------------------------------------------------------------
+    for i in range(28, len(training_set)):
+        X_train.append(training_set_scaled[i - 28:i, 0])
+        y_train.append(training_set_scaled[i, 0])
+        X_Array = np.array(X_train)
+        y_Array = np.array(y_train)
+
+    # --------------------------------------------------------------------
+    # Long Short-Term Memory (LSTM) architecture
+    # we have four LSTM layers
+    # we have one output layer
+    # --------------------------------------------------------------------
+    # Reshaping X_train for efficient modelling
+    X_Array = np.reshape(X_train, (X_Array.shape[0], X_Array.shape[1], 1))
 
     regressor = Sequential()
-    # First LSTM layer with Dropout regularisation
-    regressor.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+    # # First LSTM layer with Dropout regularisation
+    regressor.add(LSTM(units=50, return_sequences=True, input_shape=(X_Array.shape[1], 1)))
     regressor.add(Dropout(0.2))
+
     # Second LSTM layer
     regressor.add(LSTM(units=50, return_sequences=True))
     regressor.add(Dropout(0.2))
@@ -131,42 +123,43 @@ def Train_Replacement_Policy():
     regressor.add(Dense(units=1))
 
     # --------------------------------------------------------------------
-    #     Long Short-Term Memory (LSTM) architecture is a Recurrent Neural Net (RNN)
+    # Long Short-Term Memory (LSTM) architecture is a Recurrent Neural Net (RNN)
     # --------------------------------------------------------------------
 
     # Compiling the RNN
     regressor.compile(optimizer='rmsprop', loss='mean_squared_error')
     # Fitting to the training set
-    regressor.fit(X_train, y_train, epochs=5, batch_size=32)
+    regressor.fit(X_Array, y_Array, epochs=50, batch_size=50)
 
     # --------------------------------------------------------------------
-    #     Get test set ready as the training set
-    #     we get the first 100 entries of the test set have 100 previous values
+    # Get test set ready as the training set
+    # we get the first 100 entries of the test set have 100 previous values
     # --------------------------------------------------------------------
 
-    dataset_total = pd.concat((dataset["class"][:'Hit'], dataset["class"]['Miss':]), axis=0) # update to the code for cache_addr
-    inputs = dataset_total[len(dataset_total) - len(test_set) - 100:].values
-    inputs = inputs.reshape(-1, 1)
-    inputs = sc.transform(inputs)
-    # Preparing X_test and predicting the prices
+    # dataset_total = pd.concat((df, df), axis=0)
+    # print(len(dataset_total))
+    # inputs = dataset_total[len(dataset_total) - len(test_set)].values
+    # inputs = inputs.reshape(-1, 1)
+    inputs = sc.transform(test_set)
+
+    # Preparing X_test and predicting the cache index + tag
     X_test = []
     for i in range(100, 500):
         X_test.append(inputs[i - 100:i, 0])
         X_test = np.array(X_test)
         X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
         predicted_cache_result = regressor.predict(X_test)
-        predicted_cache_result = sc.inverse_transform(predicted_cache_result)
+        predicted_cache = sc.inverse_transform(predicted_cache_result)
+
     # Visualizing the results for LSTM
-    Plot_Predictions(test_set, predicted_cache_result)
+    Plot_Predictions(test_set, predicted_cache)
     # evaluate model
-    Return_RMSE(test_set, predicted_cache_result)
+    Return_RMSE(test_set, predicted_cache)
 
 
-def ReplacementPolicyDecision():
+# def ReplacementPolicyDecision():
 
 def main():
-    ConvertData()
-
     Train_Replacement_Policy()
 
 
